08571a35f040d71896b5bcd5b48c514aa879d983
Elliot Wood <gigaflare_elliot@hotmail.com>
2026-08-01 16:16:49 +1000

    teams-tab dist: shell, markdown in chat, user messages restored
    Placeholder config; the VM substitutes on install.

---
+++ assets/swimlanesDiagram-G3AALYLV-BKyxS-RP.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./src-BgY8j6MH.js";import"./chunk-WYO6CB5R-BEiOLsZs.js";import"./chunk-ICXQ74PX-AJkcu0we.js";import"./chunk-HOUHSVGY-DSpZ5qnc.js";import"./chunk-Q4XR5HBZ-CODsie7-.js";import"./chunk-7BUUIJ7U-Bb538aSH.js";import"./chunk-OGEWGWER-C-GsVzGW.js";import"./chunk-32BRIVSS-Cf5q-YiX.js";import"./chunk-XXDRQBXY-BLEyvdiM.js";import"./chunk-VR4S4FIN-D1OFtQTU.js";import"./chunk-C7G6YPKG-Cdn8iGyf.js";import"./chunk-ZGVPDNZ5-Cc0ZxQe1.js";import"./chunk-52WLFC77-DV4sjG6i.js";import"./chunk-FWX5IMBZ-vczM3ydY.js";import"./chunk-ZIRB5QZD-C6fEPe3t.js";import{r as t,t as n}from"./chunk-PUDLZKDR-BN8Idm2E.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
--- assets/swimlanesDiagram-G3AALYLV-wHXJKpdB.js
+++
@@ -1,8 +0,0 @@
-import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./src-CsUZtG8U.js";import"./chunk-WYO6CB5R-CqSQORZF.js";import"./chunk-ICXQ74PX-BX-vPnIX.js";import"./chunk-HOUHSVGY-B8J51Jz9.js";import"./chunk-Q4XR5HBZ-C_F1VtsO.js";import"./chunk-7BUUIJ7U-Bb538aSH.js";import"./chunk-OGEWGWER-CBLs5dJk.js";import"./chunk-32BRIVSS-zBlXx5ke.js";import"./chunk-XXDRQBXY-dIZXt-qo.js";import"./chunk-VR4S4FIN-CvSMvDX4.js";import"./chunk-C7G6YPKG-Cm9I3wzR.js";import"./chunk-ZGVPDNZ5-CydQvTwg.js";import"./chunk-52WLFC77-Bt319LoG.js";import"./chunk-FWX5IMBZ-BOazTuZB.js";import"./chunk-ZIRB5QZD-C6fEPe3t.js";import{r as t,t as n}from"./chunk-PUDLZKDR-D2jQ-Yvm.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
-  .swimlane.cluster rect {
-    stroke: ${e.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,`getStyles`)});export{r as diagram};
